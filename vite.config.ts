a1f446782d7e6b20b4e1041a0d7de74e3e6ba091
Sharan <sbrsharan@gmail.com>
2023-07-31 14:45:08 +0530

    first-commit.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/where-in-the-world.io",
+  base: "/where-in-the-world",
   plugins: [react()],
 })
